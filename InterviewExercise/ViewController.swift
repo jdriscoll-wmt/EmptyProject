c98ea1f5ee53ff0604d9882b600bef57d123fea2
Justin Driscoll <justin.driscoll@walmart.com>
2022-05-03 17:56:17 -0400

    Initial commit

---
+++ InterviewExercise/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  InterviewExercise
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
